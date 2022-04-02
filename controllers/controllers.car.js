c4a2240e054efa2cec36087172051352fa278090
tincho-sudo <martin8803@gmail.com>
2022-04-02 00:55:55 -0300

    added new entity's routes and finished its controller

---
+++ controllers/controllers.car.js
@@ -0,0 +1,52 @@
+const { Car } = require("../models");
+
+const newCar = async (req,res) =>{
+
+    const { brand, model, year, plate }= req.body;
+
+    let modelTrim = model.trim().toLowerCase();
+
+    try{
+        const newCar = new Car({ brand, model: modelTrim, year,plate});
+        await newCar.save();
+        res.status(200).json({newCar});
+
+    }catch(err){
+        res.status(500).json({err});
+    }
+
+};
+
+const modifyCar = async (req,res)=>{
+
+    const { brand, model, year, plate }= req.body;
+
+    const car = await Car.findById(req.car._id);
+    if(brand) car.brand = car;
+    if(model) car.model = model;
+    if(year) car.year = year;
+    if(plate) car.plate = plate;
+
+    const editedCar = await car.save();
+    res.status(200).json({editedCar});
+
+};
+
+const getCars = async (_, res)=>{
+
+    const cars = await Car.find();
+    return res.status(200).json(cars);
+};
+
+const getCar = async (req, res)=>{
+    const car = await UserModel.findById(req.params.id);
+    if (!car) return res.status(404).send("Car not defined");
+    res.status(200).send(car);
+};
+
+module.exports = {
+    newCar,
+    modifyCar,
+    getCars,
+    getCar,
+};
--- routes/routes.car.js
+++ routes/routes.car.js
@@ -1,9 +1,9 @@
-const { getCar, modifyCar, deleteCar, newCar, getAllCars } = require("../controllers/controllers.car");
+const { getCar, modifyCar, newCar, getCars } = require("../controllers/controllers.car");
 
 const routes = (app) => {
   app.route("/car/:id").get(getCar).put(modifyCar).delete(deleteCar);
 
-  app.route("/cars/").get(getAllCars).post(newCar);
+  app.route("/cars/").get(getCars).post(newCar);
 
   app.get("*", (req, res) => {
     res.status(404).send({ url: req.originalUrl + " not found" });
